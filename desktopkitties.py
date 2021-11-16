6cf6beaa1073c9c85f2ea25b04132d7b5825537a
Lord-Tytler <btytler411@gmail.com>
2021-11-16 13:07:20 -0800

    Version 0.2 -now has different states      -states can be changed immediately      -can also be queued to change at the end of current animation cycle      -active gif autmatically swaps when state changes

--- desktopkitties.py
+++ desktopkitties.py
@@ -1,18 +1,35 @@
 import tkinter as tk
 import time
 from PIL import Image, ImageTk
-from PIL import GifImagePlugin
 import pygame.time
+from enum import IntEnum
 
+class States(IntEnum):
+    IDLE=0
+    LEFT=1
+    RIGHT=2
+
+gifs = [
+    Image.open("idle.gif"),
+    Image.open("run_left.gif"),
+    Image.open("run_right.gif"),
+]
+
+#window setup
 window = tk.Tk()
 window.config(bg='') #sets background to transparent
 window.overrideredirect(True) #hides top bar
 window.wm_attributes('-transparentcolor','blue') #makes anything that is BLUE transparent
 
+#main fps and animation frame setup
 clock = pygame.time.Clock() #use to set program fps
 gameFrames = 0; #counting frames to regulate animation speed.
-gif = Image.open("run.gif")
-animationFrame = -1
+animationFrame = -1 #default animation frame, when next frame is called
+#gif = Image.open("run.gif")
+activeGif = gifs[0]
+activeState = States.IDLE
+stateQueued = False
+nextState = States.IDLE
 
 #adds a passed iamge from nextFrame() to a label and places it on the top level
 def showImage(frame):
@@ -29,31 +46,50 @@ def showImage(frame):
 #check which frame the animation should current be on, extracts and sends the next from to showImage() and returns updated frame number (previous + 1)
 def nextFrame():
     global animationFrame
-    if animationFrame >= gif.n_frames - 1:
-        animationFrame = -1
-    gif.seek(animationFrame + 1)
-    rgba = gif.convert('RGBA')
+    if animationFrame >= activeGif.n_frames - 1:
+        if stateQueued and nextState != activeState:
+            changeState(nextState)
+        else:
+            animationFrame = -1
+    activeGif.seek(animationFrame + 1)
+    rgba = activeGif.convert('RGBA')
     rgba = rgba.resize((200, 200), Image.BOX)
     frameImg = ImageTk.PhotoImage(rgba)
     showImage(frameImg)
     return animationFrame + 1
 
+#Given a desired stateIndex, sets activeState = stateIndex and sets activeGif to corresponding gif
+def changeState(stateIndex):
+    global activeGif
+    global activeState
+    global stateQueued
+    global animationFrame
+    activeState = stateIndex
+    activeGif = gifs[stateIndex]
+    animationFrame = -1
+    stateQueued = False
 
-dragging = False;
+#saves a given stateIndex into nextState and sets stateQueued=true so that the state and active gif will be changed after the current animation has finished
+def queueStateChange(stateIndex):
+    global stateQueued
+    global nextState
+    nextState = stateIndex
+    stateQueued = True
+
+#declares dragging state and offsets to default values (to be used for moving the window/cat)
+dragging = False; 
 offsetx = 0;
 offsety = 0;
 
-#when first clicked, saves windows offset from cursor, then sets the window to that same offset as cursor moves such that user can "grab" any spot on window
+#if dragging is true (see startMove() and stopMove()), sets the top level's position to a prerecorded offset of the cursors current position
 def move():
-    #global dragging
-    #global offsetx
-    #global offsety
     if dragging:
         x, y = window.winfo_pointerxy()
         x -= offsetx
         y -= offsety
         window.geometry(f"+{x}+{y}")
 
+#saves top level xy offset from cursor and sets dragging to true so move() runs
 def startMove(event):
     global dragging
     global offsetx
